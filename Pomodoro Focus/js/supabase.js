6f0a22edbc6040468939bbb0652b049891bc062d
Beatriz Teodoro <109802126+BeaTeodoro@users.noreply.github.com>
2025-11-04 08:35:48 -0300

    ajustes locais antes de pull --rebase

--- Pomodoro Focus/js/supabase.js
+++
@@ -1,6 +0,0 @@
-import { createClient } from "https://esm.sh/@supabase/supabase-js@2";
-
-const SUPABASE_URL = "https://tnwhdkspowoamnrhhgok.supabase.co";
-const SUPABASE_ANON_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InRud2hka3Nwb3dvYW1ucmhoZ29rIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjIwMDc0OTIsImV4cCI6MjA3NzU4MzQ5Mn0.avJV39Jsqvrq9vpFqLfgz12o80fwrLskBu0jCAZ4Mz8";
-
-export const supabase = createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
